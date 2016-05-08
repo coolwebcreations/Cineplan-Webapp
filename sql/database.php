0b6c6bc66fedd5891d91840e2f06fbabbf8ed768
Mike Brandner <mike.brandner@homepagewerkstatt.de>
2016-05-08 23:43:28 +0200

    Create database.php

---
+++ sql/database.php
@@ -0,0 +1,20 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+$conn = new mysqli("myServer", "myUser", "myPassword", "Northwind");
+
+$result = $conn->query("SELECT movie, showdate, showtime FROM cineplan_shows");
+
+$outp = "";
+while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+    if ($outp != "") {$outp .= ",";}
+    $outp .= '{"movie":"'  . $rs["movieid"] . '",';
+    $outp .= '"showdate":"'   . $rs["showdate"]        . '",';
+    $outp .= '"showtime":"'. $rs["showtime"]     . '"}'; 
+}
+$outp ='{"records":['.$outp.']}';
+$conn->close();
+
+echo($outp);
+?>
